3fac41eee3e5c72ba4e3ce935b11005482b3e2af
Agam Pandey <agampandey705@gmail.com>
2025-02-21 20:34:31 +0530

    Mongo async

--- mongoDB/connectDB.js
+++ mongoDB/connectDB.js
@@ -1,9 +1,12 @@
 import mongoose from "mongoose";
 
-const connectDB = (url) => {
+const connectDB = async (url) => {
     mongoose.set("strictQuery", true);
   
-    mongoose.connect(url)
+    await mongoose.connect(url, {
+        serverSelectionTimeoutMS: 30000,
+        socketTimeoutMS: 45000, 
+    })
       .then(() => console.log("MongoDB connected"))
       .catch((err)=> console.log(err)
       );
