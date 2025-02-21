f59de879f8fd629d49507a66f2e6280a6fed725a
Agam Pandey <agampandey705@gmail.com>
2025-02-21 19:59:54 +0530

    Mongo up

--- mongoDB/connectDB.js
+++ mongoDB/connectDB.js
@@ -1,17 +1,13 @@
 import mongoose from "mongoose";
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("MongoDB Connected");
-  } catch (error) {
-    console.error("MongoDB connection error:", error);
-    process.exit(1);
-  }
-};
-
-export default connectDB;
+const connectDB = (url) => {
+    mongoose.set("strictQuery", true);
+  
+    mongoose.connect(url)
+      .then(() => console.log("MongoDB connected"))
+      .catch((err)=> console.log(err)
+      );
+  };
+  
+export default connectDB ; 
   
